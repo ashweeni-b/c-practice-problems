26531f5f9413527e77ca3fc87aa5bb2eef776aa7
ashweeni-b <bhardeashweeni05@gmail.com>
2025-10-02 11:47:31 +0530

    Add Problem 10 Digit or Not

---
+++ 10-character-check/digit_or_not.c
@@ -0,0 +1,30 @@
+// Problem: To check whether a character is digit or not
+
+// Logic If the character is digit i.e. char >= '0' && char <= '9', output 1 else 0
+
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter character: ");
+    scanf("%c", &ch);
+
+    printf("%d", ch >= '0' && ch <= '9');
+
+    return 0;
+}
+
+/*
+Input: 
+Enter character: *
+
+Output: 
+0
+
+Input:
+Enter-character: 4
+
+Output:
+1
+*/
