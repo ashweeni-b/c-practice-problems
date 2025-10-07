83b9a22ce1fc5d4cc96053a1dcde53aaacd3d487
ashweeni-b <bhardeashweeni05@gmail.com>
2025-10-07 21:49:28 +0530

    Updated Problem 20 Printing numbers 0 to n using for loop, n is given by the user

--- 20-printing-numbers/printing_0_to_n.c
+++ 20-printing-numbers/printing_0_to_n.c
@@ -1,6 +1,8 @@
 // Problem: To output numbers from 1 to n using while loop
 // n is given by the user
 
+// Note: This problem can be solved using while loop as well as for loop 
+
 #include <stdio.h>
 
 int main() {
@@ -9,11 +11,19 @@ int main() {
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    int i = 1;
+    int i = 0;
 
+    // Using while loop
+    printf("Using while loop: \n");
     while (i <= n) {
         printf("%d\n", i);
         i++;
+    }
+
+    // Using for loop
+    printf("Using for loop: \n");
+    for (int j = 0; j <= n; j++) {
+        printf("%d\n", j);
     }
 
     return 0;
@@ -24,6 +34,7 @@ Input 1:
 Enter the value of n: 4
 
 Output 1:
+0
 1
 2
 3
@@ -33,6 +44,7 @@ Input 2:
 Enter the value of n: 2
 
 Output 2:
+0
 1
 2
 */
