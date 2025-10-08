4d79e44f4744de3996daff744bd71ace16442ce8
ashweeni-b <bhardeashweeni05@gmail.com>
2025-10-09 02:18:55 +0530

    Update Problem 26 Sum and product of all digits of a number

---
+++ 26-operations-on-digits-of-a-number/sum_and_product_of_digits.c
@@ -0,0 +1,46 @@
+// Problem: To calculate the sum of the digits of a number
+
+/*
+Logic:
+num = 123, sum = 0, product = 1
+digit = 123 % 10 = 3     num = 123 / 10 = 12      sum = sum + digit = 3     product = product * digit = 3
+digit = 12 % 10 = 2      num = 12 / 10 = 1        sum = 5                   product = 6
+digit = 1 % 10 = 1       num = 1 / 10 = 0         sum = 6                   product = 6
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    int digit, sum = 0, product = 1;
+
+    for(int i = 0; num != 0; i++) {
+        digit = num % 10;
+        sum += digit;
+        product *= digit;
+        num /= 10;
+    }
+
+    printf("Sum of digits = %d\n", sum);
+    printf("Product of digits = %d", product);
+
+    return 0;
+}
+
+/*
+Input 1: 
+Enter the number: 123
+
+Output 1:
+Sum of digits = 6
+
+Input 2:
+Enter the number: 4332
+
+Output 2:
+Sum of digits = 12
+*/
